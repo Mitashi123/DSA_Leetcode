fc342a8d7409609ca271a33276a74867688b68d4
Mitashi Gandhi <79134485+Mitashi123@users.noreply.github.com>
2023-05-04 22:43:07 +0530

    Time: 14 ms (21.59%), Space: 43.4 MB (32.39%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> radiant = new LinkedList<>(), dire = new LinkedList<>();
+        for(int i = 0; i < n; ++i){
+            if(senate.charAt(i) == 'R') radiant.add(i);
+            else dire.add(i);
+        }
+        if(radiant.isEmpty()) return "Dire";
+        else if(dire.isEmpty()) return "Radiant";
+        while(!radiant.isEmpty() && !dire.isEmpty()){
+            if(radiant.peek() < dire.peek()){
+                dire.remove();
+                int rad = radiant.poll();
+                radiant.add(n + rad);
+            } else{
+                radiant.remove();
+                int dir = dire.poll();
+                dire.add(n + dir);
+            }
+        }
+        return radiant.isEmpty() ? "Dire": "Radiant";
+    }
+}
